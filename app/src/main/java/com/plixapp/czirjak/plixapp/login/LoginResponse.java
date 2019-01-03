b57957e572b59aa0157f7a565319ed7d806aba7a
martonczirjakhome <marton.czirjak@mmsone.com>
2019-01-03 23:05:54 +0100

    Login works

---
+++ app/src/main/java/com/plixapp/czirjak/plixapp/login/LoginResponse.java
@@ -0,0 +1,8 @@
+package com.plixapp.czirjak.plixapp.login;
+
+class LoginResponse {
+    //TODO DATAN belül a Hunter nullként jön sikeres belépésnél is
+
+    public LoginResponse() {
+    }
+}
--- app/src/main/java/com/plixapp/czirjak/plixapp/login/PlixAppLoginActivity.kt
+++ app/src/main/java/com/plixapp/czirjak/plixapp/login/PlixAppLoginActivity.kt
@@ -4,6 +4,9 @@ import android.database.DatabaseUtils
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.android.volley.Response
 
 import com.plixapp.czirjak.plixapp.R
 import com.plixapp.czirjak.plixapp.databinding.ActivityLoginBinding
@@ -13,11 +16,21 @@ import com.plixapp.czirjak.plixapp.databinding.ActivityLoginBinding
  */
 class PlixAppLoginActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
-
+        binding.emailSignInButton.setOnClickListener {
+            LoginRequest(
+                binding.email.text.toString(),
+                binding.password.text.toString(),
+                Response.Listener { succesResponse ->
+                    Toast.makeText(this, "Sikeres belépés.", Toast.LENGTH_LONG).show()
+                },
+                Response.ErrorListener { response ->
+                    Toast.makeText(this, "Hiba", Toast.LENGTH_LONG).show()
+                }).send(this)
+        }
     }
 }
